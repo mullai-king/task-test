073cb99a81425eb3109459310961ae1eec8c6587
mullai-king <52599344+mullai-king@users.noreply.github.com>
2023-11-23 20:11:30 +0530

    day3-task

--- day-3/compareJSON.js
+++ day-3/compareJSON.js
@@ -1,1 +1,21 @@
-//write 
+let obj1 = {name: "person1",age:5}
+let obj2 = {age:5,name: "person1"}
+ if(Object.keys(obj1).length==Object.keys(obj2).length){
+     for(var key in obj1){
+         if(obj1[key]==obj2[key]){
+         isequal=true;
+         }
+         else{
+             console.log("type of property different")
+             isequal=false;
+         }
+     }
+   }
+ 
+   else{
+         console.log("No of property different")
+         isequal=false;
+   }
+   if(isequal==true){
+     console.log("all properties are same")
+   }
