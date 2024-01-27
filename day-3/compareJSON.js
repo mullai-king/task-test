4f075bbeba0fa808d09a87360062a4027d9bb804
mullai-king <52599344+mullai-king@users.noreply.github.com>
2024-01-27 15:47:12 +0530

    learned local scope variable

--- day-3/compareJSON.js
+++ day-3/compareJSON.js
@@ -1,7 +1,7 @@
 let obj1 = {name: "person1",age:5}
 let obj2 = {age:5,name: "person1"}
  if(Object.keys(obj1).length==Object.keys(obj2).length){
-     for(var key in obj1){
+     for(let key in obj1){
          if(obj1[key]==obj2[key]){
          isequal=true;
          }
