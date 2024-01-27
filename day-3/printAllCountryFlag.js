b9499045c8e4f2ecbd2b96bb5578351a83f49747
mullai-king <52599344+mullai-king@users.noreply.github.com>
2024-01-27 15:41:57 +0530

    task resummition using xmhttp request

--- day-3/printAllCountryFlag.js
+++ day-3/printAllCountryFlag.js
@@ -1,6 +1,15 @@
-fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => countryFlag(msg))
-function countryFlag(msg){
- for(var x =0;msg.length>x;x++){
-  console.log(msg[x].flag)
- } 
-}
+// fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => countryFlag(msg))
+
+const xhr = new XMLHttpRequest();
+URL = "https://restcountries.com/v3.1/all";
+xhr.open("GET",URL);
+xhr.send();
+xhr.onload = ()=>{
+    let msg = JSON.parse(xhr.response);
+    for(let x =0;msg.length>x;x++){
+     
+      console.log(msg[x].flag+" And countries names =>"+msg[x].name.common)
+ 
+    }
+  }
+ 
--- day-3/printCountryDetailsAndPopulation.js
+++ day-3/printCountryDetailsAndPopulation.js
@@ -1,6 +1,18 @@
-fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => countryFlag(msg))
-function countryFlag(msg){
- for(var x =0;msg.length>x;x++){
-  console.log("country is "+msg[x].name.common+", Region is "+msg[x].region+", Sub Region is "+msg[x].subregion+", Papulation is "+msg[x].population)
- } 
+// fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => countryFlag(msg))
+// function countryFlag(msg){
+//  for(var x =0;msg.length>x;x++){
+//   console.log("country is "+msg[x].name.common+", Region is "+msg[x].region+", Sub Region is "+msg[x].subregion+", Papulation is "+msg[x].population)
+//  } 
+// }
+
+const xhr =new XMLHttpRequest();
+const URL ="https://restcountries.com/v3.1/all"
+
+xhr.open("GET",URL);
+xhr.send();
+xhr.onload =() =>{
+  let msg =JSON.parse(xhr.response)
+  for(let x=0;msg.length >x;x++){
+    console.log("Name: "+msg[x].name.common+" Region: "+msg[x].region +" Subregion: "+msg[x].subregion+" Population: "+msg[x].population)
+  }
 }
