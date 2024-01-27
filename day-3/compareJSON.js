cac3572c5eabada79ae96f0723cdfe4037f26bd4
mullai-king <52599344+mullai-king@users.noreply.github.com>
2024-01-27 19:20:11 +0530

    write logic for diffrent key value

--- day-3/compareJSON.js
+++ day-3/compareJSON.js
@@ -1,21 +1,25 @@
 let obj1 = {name: "person1",age:5}
-let obj2 = {age:5,name: "person1"}
+let obj2 = {age:2,name: "person2",as:2}
+let count = 0;
+let mismatch =0;
+let isequal = false;
  if(Object.keys(obj1).length==Object.keys(obj2).length){
      for(let key in obj1){
          if(obj1[key]==obj2[key]){
-         isequal=true;
+         count++;
          }
          else{
-             console.log("type of property different")
-             isequal=false;
+             mismatch++
          }
      }
    }
  
    else{
-         console.log("No of property different")
-         isequal=false;
+         console.log("No of object key is different")
    }
-   if(isequal==true){
+   if(Object.keys(obj1).length == count){
      console.log("all properties are same")
+   }
+   else if(mismatch > 0 ){
+    console.log(mismatch +" object key values is diffrent")
    }
