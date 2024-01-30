62959b46c8b462252e338814afb27efe839c1516
mullai-king <52599344+mullai-king@users.noreply.github.com>
2024-01-31 00:54:54 +0530

    day-5 task completion

--- day-5/UseJSONfileLoops.js
+++ day-5/UseJSONfileLoops.js
@@ -1,30 +1,49 @@
-fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => countryFlag(msg))
-function countryFlag(msg){
-console.log("usig for loop print all country flag")
- for(var x =0;msg.length>x;x++){
-  console.log(msg[x].flag)
- } 
+//storing variables 
+const xmlReq = new XMLHttpRequest();
+const URL = 'https://restcountries.com/v3.1/all';
+//strat fetching from api address
+xmlReq.open("GET",URL)
+xmlReq.send();
+xmlReq.onload = ()=>{
+   let apiRes =JSON.parse(xmlReq.response)
+   printAllCountryFlag(apiRes);
+   printAllAsianCountries(apiRes);
+   populationBillionCountry(apiRes);
 }
-fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => countryCommonName(msg))
-function countryCommonName(msg){
-console.log("usig forin loop print all asian country official name")
- for(var x in msg){
- if(msg[x].region == "Asia")
-    console.log(msg[x].name.official)
- }
-} 
-fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => countrypapulation(msg))
-function countrypapulation(msg){
-console.log("usig for of loop print  population morethan 1 billion")
- for(var x of msg){
- if(x.population>1000000000)
-    console.log(x.name.official)
- }
+
+//Written funcion print all country flag using "for normal loop"
+function printAllCountryFlag(apiRes){
+   for(let x=0;apiRes.length>0;x++){
+      //here just printing output CL. if it require we can store [arr] and return the [arr].
+      console.log(apiRes[x].flag)
+   }
+   
 }
-fetch('https://restcountries.com/v3.1/all').then((res) =>res.json()).then((msg) => nameCapitalFlag(msg))
-function nameCapitalFlag(msg){
-console.log("usig forEach loop print all country name,capital,flag")
- msg.forEach((x)  => {
- console.log("country: "+x.name.official+", capital :"+x.capital +", flag:"+x.flag)
- })
+
+//Written function all asian countries using "for of loop" and anonymous function 
+let printAllAsianCountries =(apiRes) =>{
+   for(let value of apiRes){
+      if(value.continents.includes("Asia")){
+         console.log(value.name.common)
+      }
+   }
+}
+
+//written function to get morethan 1Billion population "use for in loop"
+function populationBillionCountry(apiRes){
+   for(let ind in apiRes){
+      if(apiRes[ind].population > 1000000000){
+         console.log(apiRes[ind].name.common)
+      }
+   }
+}
+
+//written function to print all country name ande currency name and currency symbol
+let printAllCountriesNameAndCurrency = (apiRes) =>{
+   apiRes.forEach(element => {
+   	let [currencycode] = Object.keys(element.currencies);
+      let [currency,currencySymbol]= Object.values(element.currencies[currencycode]);
+      
+      console.log(element.name.common + "Currency Name "+ currency+"Currency Symbol " +currencySymbol)
+   });
 }
